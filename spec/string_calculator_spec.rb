e50aa67058650cd698ddc4a5a3e933d828864f50
Shubham Rathore <shubhamr0131@gmail.com>
2024-07-28 12:32:24 +0530

    Finally split the positive and negative cases

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,52 +4,56 @@ RSpec.describe StringCalculator do
   subject(:calculator) { described_class }
 
   describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(calculator.add("")).to eq(0)
+    context 'with valid inputs' do
+      it 'returns 0 for an empty string' do
+        expect(calculator.add("")).to eq(0)
+      end
+
+      it 'returns the number for a single number' do
+        expect(calculator.add("1")).to eq(1)
+      end
+
+      it 'returns the sum of two numbers' do
+        expect(calculator.add("1,2")).to eq(3)
+      end
+
+      it 'returns the sum of multiple numbers' do
+        expect(calculator.add("1,2,3")).to eq(6)
+      end
+
+      it 'handles new lines between numbers' do
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
+
+      it 'supports different delimiters' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+
+      it 'ignores numbers bigger than 1000' do
+        expect(calculator.add("2+1001")).to eq(2)
+      end
+
+      it 'supports delimiters of any length' do
+        expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+      end
+
+      it 'supports multiple delimiters' do
+        expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
+      end
+
+      it 'supports multiple delimiters with length longer than one char' do
+        expect(calculator.add("//[***][%%%]\n1***2%%%3")).to eq(6)
+      end
     end
 
-    it 'returns the number for a single number' do
-      expect(calculator.add("1")).to eq(1)
-    end
+    context 'with invalid inputs' do
+      it 'throws an exception for negative numbers' do
+        expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+      end
 
-    it 'returns the sum of two numbers' do
-      expect(calculator.add("1,2")).to eq(3)
-    end
-
-    it 'returns the sum of multiple numbers' do
-      expect(calculator.add("1,2,3")).to eq(6)
-    end
-
-    it 'handles new lines between numbers' do
-      expect(calculator.add("1\n2,3")).to eq(6)
-    end
-
-    it 'supports different delimiters' do
-      expect(calculator.add("//;\n1;2")).to eq(3)
-    end
-
-    it 'throws an exception for negative numbers' do
-      expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
-    end
-
-    it 'throws an exception for multiple negative numbers' do
-      expect { calculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2, -3")
-    end
-
-    it 'ignores numbers bigger than 1000' do
-      expect(calculator.add("2+1001")).to eq(2)
-    end
-
-    it 'supports delimiters of any length' do
-      expect(calculator.add("//[***]\n1***2***3")).to eq(6)
-    end
-
-    it 'supports multiple delimiters' do
-      expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
-    end
-
-    it 'supports multiple delimiters with length longer than one char' do
-      expect(calculator.add("//[***][%%%]\n1***2%%%3")).to eq(6)
+      it 'throws an exception for multiple negative numbers' do
+        expect { calculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2, -3")
+      end
     end    
   end
 end
